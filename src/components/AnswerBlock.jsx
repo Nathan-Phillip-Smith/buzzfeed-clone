2bce231680be46b60b820ffab89c3a9bccf89387
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-08-29 08:26:47 -0600

    removed fake backend

--- src/components/AnswerBlock.jsx
+++ src/components/AnswerBlock.jsx
@@ -1,27 +1,28 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
-const AnswerBlock = ({answerOptions, chosenAnswers}) => {
-    const [result, setResult] = useState(null)
+const AnswerBlock = ({ answerOptions, chosenAnswers }) => {
+  const [result, setResult] = useState(null)
 
-    useEffect(() => {
-        answerOptions.forEach(answer => {
-            if (chosenAnswers.includes(answer.combination[0]) &&
-                chosenAnswers.includes(answer.combination[1]) &&
-                chosenAnswers.includes(answer.combination[2]) 
-                ){
-                    setResult(answer)
-                } 
-        }) 
-    }, [])
+  useEffect(() => {
+    answerOptions.forEach((answer) => {
+      if (
+        chosenAnswers.includes(answer.combination[0]) &&
+        chosenAnswers.includes(answer.combination[1]) &&
+        chosenAnswers.includes(answer.combination[2])
+      ) {
+        setResult(answer)
+      }
+    })
+  })
 
-    console.log(result)
+  console.log(result)
 
-    return (
-        <div id="answer-block" className="answer-block">
-            <h2>{result?.text}</h2>
-            <img src={result?.image} alt={result?.text}></img>
-        </div>
-    )
+  return (
+    <div id="answer-block" className="answer-block">
+      <h2>{result?.text}</h2>
+      <img src={result?.image} alt={result?.text}></img>
+    </div>
+  )
 }
 
 export default AnswerBlock
